c3192b6a41dc91c3188e704cb6e77f9c1945ca54
rustamspl <rustamspl@gmail.com>
2017-02-12 21:44:56 +0600

    1

---
+++ src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="login">
+        <h3>
+            Вход
+        </h3>
+        <form  @submit.prevent="doLogin">
+            <div class="row">
+                <input placeholder="Логин" type="text" v-model="login" name="login" />
+            </div>
+            <div class="row">
+                <input placeholder="Пароль" type="password" v-model="pass"/>
+            </div>
+            <div class="control">
+                <button type="submit">
+                    Вход
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+    var data={
+        login: 'aaa',
+        pass: '',
+        doLogin () {
+          console.log(data.login);
+        }
+     };
+    export default {
+    data(){ return data }
+}
+</script>
+<style scoped="">
+.login{
+  width: 200px;
+  height: 300px;
+  margin: auto auto;
+}
+.row,.control{
+  padding: 5px;
+}
+.control{
+
+}
+input{
+  width:150px;
+  margin: 0;
+
+}
+</style>
